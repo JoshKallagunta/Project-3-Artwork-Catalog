2929d98156e2c72533f8525c647eb7ca0dc41c87
joshkallagunta <nq6647xv@go.minneapolis.edu>
2019-10-03 16:42:00 -0500

    Add Comments

--- Documents/project3Artwork/inputAddArtwork.py
+++ Documents/project3Artwork/inputAddArtwork.py
@@ -2,6 +2,8 @@ from peewee import *
 from artworkCatalog import (ArtistData,ArtworkData)
 import dbArtwork 
 
+#Method for adding an artist, getting user input, and saving to the ArtistData table
+#called in main.py class for the UI 
 def adding_artist_from_user():
     try:
         artistName = input('Enter an artist''s name: ')
@@ -10,6 +12,8 @@ def adding_artist_from_user():
     except:
         print("This Artist has already been entered")
 
+#Method for adding an artwork, getting user input, saving to ArtworkData table 
+#called in main.py class for the UI 
 def adding_new_artwrk_from_user():
     try:
         album_artist = input('Enter the artist''s name for the album: ')
@@ -20,6 +24,9 @@ def adding_new_artwrk_from_user():
     except:
         print('This artwork already exixsts, please enter a unique one. ')
 
+#Method for deleting an artwork, getting the user input for which artwork to be deleted
+#Deleted from the ArtworkData table
+#called in main.py class for the UI 
 def deleting_artwork_from_user():
     try:
         album_name = input('Enter the artwork name you want to delete: ')
--- Documents/project3Artwork/main.py
+++ Documents/project3Artwork/main.py
@@ -3,6 +3,9 @@ from artworkCatalog import (ArtistData,ArtworkData)
 import dbArtwork 
 import inputAddArtwork
 
+
+#While true loop using if/elif statements for the UI menu 
+#based off of reading_list menu
 def main():
 
     while True:
@@ -18,20 +21,20 @@ def main():
 
 
 
-
-
     return user_input_choice
 
 
-
+#Menu that the user sees, gets input based off the number, returns the user choice so that the 
+#main method will execute what the user chose 
 def create_menu():
-    print('Add a new artist ')
-    print('Add a new artwork ')
-    print('Delete an artwork ')
+    print('1: Add a new artist ')
+    print('2: Add a new artwork ')
+    print('3: Delete an artwork ')
 
     get_user_choice = int(input('Please enter your choice: '))
 
     return get_user_choice
+
 
 
 if __name__ == '__main__':
